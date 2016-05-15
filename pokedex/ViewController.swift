0b66e3be0fad43fb14a1037980151ccd15f7f9c9
Bartosz Odrzywołek <Bart@Bartoszs-MacBook-Pro.local>
2016-05-15 23:46:03 +0200

    Fix issues with filtering

--- pokedex/ViewController.swift
+++ pokedex/ViewController.swift
@@ -29,6 +29,7 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         collection.delegate = self
         collection.dataSource = self
         searchBar.delegate = self
+        searchBar.returnKeyType = UIReturnKeyType.Done
         
         setSearchBarTextColor()
         initAudio()
@@ -139,10 +140,16 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         }
     }
     
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        view.endEditing(true)
+    }
+    
     func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
         
         if searchBar.text == nil || searchBar.text == "" {
             inSearchMode = false
+            view.endEditing(true)
+            collection.reloadData()
         } else {
             inSearchMode = true
             let lower = searchBar.text!.lowercaseString
